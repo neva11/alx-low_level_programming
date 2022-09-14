867e956e1538c97946cbe7ee53f815f01e7bba96
nebiye <lioulnebiye@gmail.com>
2022-09-14 05:18:38 -0700

    print putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,13 +6,13 @@
  */
 int main(void)
 {
-	char str[] = "_putchar\n";
-	int i = 0;
-	while (str[i] != '\0')
+char str[] = "_putchar\n";
+int i = 0;
+while (str[i] != '\0')
 	{
-		char c = str[i];	
+		char c = str[i];
 		_putchar(c);
 		i++;
 	}
-	return (0);
+return (0);
 }
