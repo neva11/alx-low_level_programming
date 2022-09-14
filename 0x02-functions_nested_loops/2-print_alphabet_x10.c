b17e8656c1a6ca97b4869eb4220dae4c9c90f2f4
nebiye <lioulnebiye@gmail.com>
2022-09-14 05:53:36 -0700

    print alphabet 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,14 +8,13 @@
 void print_alphabet_x10(void)
 {
 	int n = 0;
-	
 	while (n < 10)
 	{
 		char c = 'a';
-	
-		while(c <= 'z')
+		while (c <= 'z')
 		{
 		_putchar(c);
+		c++;
 		}
 		_putchar('\n');
 		n++;
