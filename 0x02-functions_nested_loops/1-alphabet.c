c241c760920f32b9c08b68a7bd6fff15f1b3a401
nebiye <lioulnebiye@gmail.com>
2022-09-14 05:32:11 -0700

    print alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print small letter numbers
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+	for(char c = 'a'; char <= 'z'; char++)
+		_putchar(c);
+	_putchar("\n");
+}
