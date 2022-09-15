6c606cdf2b81eacbdc39e3348e02cf36ace98064
nebiye <lioulnebiye@gmail.com>
2022-09-15 06:17:38 -0700

    print 0-9 except 2 and 4  using _putchar

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+int _putchar(char c);
+
 int _isupper(int c);
 
 int _isdigit(int c);
@@ -10,4 +12,5 @@ int mul(int a, int b);
 void print_numbers(void);
 
 void print_most_numbers(void);
+
 #endif
