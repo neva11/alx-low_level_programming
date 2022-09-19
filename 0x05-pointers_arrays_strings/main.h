4afa9e16b2f27f0701e351abf2e3780045ebd173
nebiye <lioulnebiye@gmail.com>
2022-09-19 13:42:49 -0700

    Write a function that prints a string, followed by a new line, to stdout

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,11 +4,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+int _putchar(char c);
+
 void reset_to_98(int *n);
 
 void swap_int(int *a, int *b);
 
-int _strlen(char *s);
+size_t _strlen(char *s);
 
 void _puts(char *str);
 
